210e12312c30cdeb4564ed4e089485529eb2c329
zebra-it <InNushell.act@yandex.ru>
2020-02-28 11:32:49 +0300

    закончили с интерфейсами

--- src/com.whmcs/FunctionalInterfc/_Suppler.java
+++ src/com.whmcs/FunctionalInterfc/_Suppler.java
@@ -1,4 +1,20 @@
 package com.whmcs.FunctionalInterfc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Supplier;
+
 public class _Suppler {
+    public static void main(String[] args) {
+        System.out.println(getDBConnectionUrl());
+        System.out.println(getDBConnectionUrlSupplier.get());
+
+    }
+
+    static String getDBConnectionUrl() {
+        return "jdbc://localhost:4683:/users";
+    }
+
+    static Supplier<String> getDBConnectionUrlSupplier = () ->
+            "jdbc://localhost:4683:/users";
 }
